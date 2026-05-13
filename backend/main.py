78a8173bd3140da3ef85b446373d35532b30ed4e
Karan Tripathi <karan.tripathi@cloudsufi.com>
2026-05-14 01:35:21 +0530

    updated cors

--- backend/main.py
+++ backend/main.py
@@ -62,6 +62,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "https://project-23pz9.vercel.app"
     ],
+    allow_origin_regex=r"https://.*\.vercel\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
